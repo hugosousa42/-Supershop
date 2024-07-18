dc76b85d7e35c0005b33f54637a52dad4105a0d3
hugosousa42 <hugosousa42@proton.me>
2024-07-18 20:01:53 +0000

    First Controller

---
+++ Supershop/Supershop/Data/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supershop.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = false)]
+        public double stock { get; set; }
+    }
+}
