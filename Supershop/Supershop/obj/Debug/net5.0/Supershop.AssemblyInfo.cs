c2a7b38eea32b78fb7a556bd68daf8f1de0f5eed
hugosousa42 <hugosousa42@proton.me>
2024-08-26 06:25:28 +0000

    Modify products.

--- Supershop/Supershop/obj/Debug/net5.0/Supershop.AssemblyInfo.cs
+++ Supershop/Supershop/obj/Debug/net5.0/Supershop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supershop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01855f860a29fceb22b99b7f47263261cbd3ee5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc76b85d7e35c0005b33f54637a52dad4105a0d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supershop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supershop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
