4b66712bfdd9b7240294b21c0ae276eb1730fcce
hugosousa42 <hugosousa42@proton.me>
2024-09-21 17:01:22 +0000

    Revert ".gitignore changes"
    This reverts commit cec5d9a1d225bc5f25c402f293de178e15659f84.

--- Supershop/Supershop/obj/Debug/net5.0/Supershop.AssemblyInfo.cs
+++ Supershop/Supershop/obj/Debug/net5.0/Supershop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supershop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fdaf7e8d54fd4b6ba9d680a767c032f8bed2ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679bb4d33aa897ed2ac0bda1b7ffa3f376a82f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supershop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supershop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
